515fcb500dac5fa0bd915546aa800361f6a9adaf
watersjason <jason.waters@nist.gov>
2019-05-13 12:01:47 -0400

    Update __init__.py

--- pymeasure/instruments/agilent/__init__.py
+++ pymeasure/instruments/agilent/__init__.py
@@ -28,6 +28,7 @@ from .agilentE4408B import AgilentE4408B
 from .agilentE4980 import AgilentE4980
 from .agilent34410A import Agilent34410A
 from .agilentB2961A import AgilentB2961A
-from .agilent3458A import Agilent3458A
+# from .agilent3458A import Agilent3458A
 from .agilent53131A import Agilent53131A
 from .agilent53131A import Agilent53131A
+from .agilent34970A import Agilent34970A
