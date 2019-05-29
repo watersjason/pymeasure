068d54ed10c14038d3121962b2ad62f021c9149a
watersjason <jason.waters@nist.gov>
2019-05-29 07:59:25 -0400

    Update keithley6514.py

--- pymeasure/instruments/keithley/keithley6514.py
+++ pymeasure/instruments/keithley/keithley6514.py
@@ -28,7 +28,8 @@ log.addHandler(logging.NullHandler())
 
 from pymeasure.instruments import Instrument
 from pymeasure.instruments.validators import (strict_range,
-                                              strict_discrete_set)
+                                              strict_discrete_set,
+                                              truncated_range)
 
 from .buffer import KeithleyBuffer
 
@@ -78,7 +79,7 @@ class Keithley6514(Instrument):
     )
     calculate_state=Instrument.control(
         "CALC:STAT?",
-        "CALC:STAT %b",
+        "CALC:STAT %i",
         """ Query/set the calculate state. """,
         validator=strict_discrete_set,
         values=(0,1),
@@ -94,10 +95,13 @@ class Keithley6514(Instrument):
         """ Query and return calculation the single latest result from
             the measurements collected since the last trigger. """
     )
-    # skip CALC2 commands
+    # CALCulate3
     calculate_statistics_format=Instrument.control(
         "CALC3:FORM?",
         "CALC3:FORM %s",
+        """ A string parameter for the statistical value returned by the stats
+            register.
+            Values are: `mean`, `sdev`, `max`, `min` or `peak to peak`. """,
         validator=strict_discrete_set,
         values={'mean':'MEAN',
                 'sdev':'SDEV',
@@ -111,8 +115,94 @@ class Keithley6514(Instrument):
         "Query and return the value from the statistics register."
     )
     # DISPlay
+    display_digits=Instrument.control(
+        ":DISP:DIG?",
+        ":DISP:DIG %g",
+        """An integer parameter for the digits (4 to 7) on the front display""",
+        validator=strict_discrete_set,
+        values=list(range(4,8,1)),
+        cast=int
+    )
+    display_state=Instrument.control(
+        ":DISP:ENAB?",
+        ":DISP:ENAB %i",
+        """ A boolean parameter for the display state.
+        When `True`, the front display is enabled.""",
+        validator=strict_range,
+        values=(0,1),
+        cast=bool
+    )
     # FORMat
     # SENSe
+    sense_function=Instrument.control(
+        ":SENS:FUNC?",
+        ":SENS:FUNC %s",
+        """ A string parameter for the device sense function:
+            DC `voltage`, DC `current`, `resistance`, or `charge`. """,
+        validator=strict_discrete_set,
+        values={'voltage':'VOLT','current':'CURR',
+                'resistance':'RES','charge':'CHAR'},
+        map_values=True
+    )
+    sense_latest=Instrument.measurement(
+        ":SENS:DATA:LAT?",
+        """ A float parameter that returns the last instrument reading."""
+    )
+    sense_nplc=Instrument.control(
+        ":SENS:VOLT:NPLC?",
+        ":SENS:VOLT:NPLC %g",
+        """ A float parameter for the number of power line cycles
+            used for the measurement integration. This command affects
+            all sense functions. """,
+        validator=truncated_range,
+        values=[i/100 for i in range(1,1001,1)]
+    )
+    voltage_range=Instrument.control(
+        ":SENS:VOLT:RANG:UPP?",
+        ":SENS:VOLT:RANG:UPP %i",
+        """ An integer parameter for the measurement range. Value in volts.""",
+        validator=strict_discrete_set,
+        values=list(range(-211,211,1)),
+        cast=int
+    )
+    voltage_range_auto=Instrument.control(
+        ":SENS:VOLT:RANG:AUTO?",
+        ":SENS:VOLT:RANG:AUTO %i",
+        """ A boolean parameter for the enabled
+            state of the measurement autorange. """,
+        validator=strict_discrete_set,
+        values=(0,1),
+        cast=bool
+    )
+    voltage_range_auto_upper=Instrument.control(
+        ":SENS:VOLT:RANG:AUTO:ULIM?",
+        ":SENS:VOLT:RANG:AUTO:ULIM %f",
+        """ A float parameter for the upper value of the measurement autorange.
+            Value in volts. """,
+        validator=truncated_range,
+        values=(-210,210)
+    )
+    voltage_range_auto_lower=Instrument.control(
+        ":SENS:VOLT:RANG:AUTO:LLIM?",
+        ":SENS:VOLT:RANG:AUTO:LLIM %f",
+        """ A float parameter for the lower value of the measurement autorange.
+            Value in volts. """,
+        validator=truncated_range,
+        values=(-210,210)
+    )
+    voltage_guard=Instrument.control(
+        ":SENS:VOLT:GUAR?",
+        ":SENS:VOLT:GUAR %s",
+        """ A boolean parameter for the enabled state of the driven guard. """
+    )
+    voltage_external_feedback=Instrument.control(
+        ":SENS:VOLT:XFE?",
+        ":SENS:VOLT:XFE %i",
+        """ A boolean parameter for the enabled state of external feedback. """,
+        validator=strict_discrete_set,
+        values=(0,1),
+        cast=bool
+    )
     # SOURce
     # STATus
     # SYSTem
